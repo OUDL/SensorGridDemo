02944b5268667ad28a1529c592af4108f5a05f5c
Kyle Oba <koba@mudphone.com>
2013-06-07 11:46:50 -1000

    Make it just draw the sensor grid.

--- src/dotty/draw.clj
+++ src/dotty/draw.clj
@@ -7,4 +7,12 @@
   []
   (swap! tick inc)
   (qc/background 0 0 0)
-  (draw-sensors/draw-sensor-grid grid-sensors tick))
+  (draw-sensors/draw-sensor-grid grid-sensors tick)
+  ;; (qc/display-filter :blur 2)
+  
+  ;; (qc/translate (* 0.5 (qc/width)) (* 0.5 (qc/height)))
+  ;; (let [theta (* 0.02 @tick)
+  ;;       x (* 0.5 (qc/width) (Math/sin theta))]
+  ;;   (qc/fill 255 40)
+  ;; (qc/ellipse x 0 200 200))
+  )
